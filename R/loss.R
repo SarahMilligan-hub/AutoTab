70c0979f0a75c407f2d03452d112c43bad2b4951
SarahMilligan-hub <slm1999@bu.edu>
2026-02-08 12:52:07 -0500

    Version 0.1.3

--- R/loss.R
+++ R/loss.R
@@ -4,9 +4,8 @@
 #############################
 #' @keywords internal
 lossbasedondist = function(input, feat_dist, target,weighted=0, recon_weights){ #here the "input" data set contains the output of the VAE, target is the real data (dummy coded)
-  tf = tensorflow::tf
-
-  index_x = 0 #start at zero just as we did when defining the activation functions since tensors start at 0
+  index_x = 0L #start at zero just as we did when defining the activation functions since tensors start at 0
+  index_y = 0L
   cont_loss_LL = list()
   bin_loss_LL = list()
   cat_loss_LL = list()
@@ -18,44 +17,46 @@ lossbasedondist = function(input, feat_dist, target,weighted=0, recon_weights){ 
     num_params = variable$num_params
 
     # TRACKING: Print the current distribution and num_params and index
-    message(paste("Loss - Processing feature", index_type,
-                  "with distribution:", distribution,
-                  "and num_params:", num_params,
-                  "with index starting at", index_x))
+    print(paste("Loss - Processing feature", index_type, "with distribution:", distribution, "and num_params:", num_params, "with index starting at", index_x))
 
     if (distribution=="gaussian"){
       mean = tf$slice(input, begin=list(0L,as.integer(index_x)), size=list(tf$shape(input)[1], 1L))
       SD_raw = tf$slice(input, begin=list(0L,as.integer(index_x+1)), size=list(tf$shape(input)[1], 1L))
       SD = tf$math$softplus(SD_raw) + 1e-3 #ensure SD will be positive so continuous recon does not come out negative
 
-      ll = 0.5*tf$math$log(2*pi)+tf$math$log(SD)+0.5*tf$square(((tf$slice(target, begin=list(0L,as.integer(index_type-1)), size=list(tf$shape(input)[1], 1L)) - mean)/ SD))
+      ll = 0.5*tf$math$log(2*pi)+tf$math$log(SD)+0.5*tf$square(((tf$slice(target, begin=list(0L,as.integer(index_y)), size=list(tf$shape(input)[1], 1L)) - mean)/ SD))
       #This is the typical negative log likelihood. It pulls the true target mean, and uses the VAE output SD and mean
 
       cont_loss_LL[[index_type]]= tf$reduce_mean(ll)
-      index_x = index_x + 2 # we used up two of the indexes for this Gaussian so the next one starts at 2 over
+      index_x = index_x + 2L # we used up two of the indexes for this Gaussian so the next one starts at 2 over
+      index_y = index_y + 1L
     }
     else   if (distribution =="bernoulli"){
       prob_crude = tf$slice(input, begin=list(0L,as.integer(index_x)), size=list(tf$shape(input)[1], 1L))
       prob = tf$clip_by_value(prob_crude, eps, 1 - eps)
-      y_true= tf$slice(target, begin=list(0L,as.integer(index_type-1)), size=list(tf$shape(input)[1], 1L))
+      y_true= tf$slice(target, begin=list(0L,as.integer(index_y)), size=list(tf$shape(input)[1], 1L))
 
       ll = -(y_true*tf$math$log(prob)+(1-y_true)*tf$math$log(1-prob))
-      #This is the typical negative log likelihood of the Bernoulli distribution! We have the target probability and the predicted probability , sum is inherently handled by how tensors work
+      #This is the typical negative log likelihood of the Bernoulli distribution! We have the target probability and the predicted probability , sum is inherntly handled by how tensors work
       #ll based on logist ont probs
 
       bin_loss_LL[[index_type]]= tf$reduce_mean(ll)
-      index_x = index_x + 1 # only used one position so move over by 1
+      index_x = index_x + 1L # only used one position so move over by 1
+      index_y = index_y + 1L
     }
     else if (distribution =="categorical"){
 
       logit_crude = tf$slice(input, begin = list(0L, as.integer(index_x)), size = list(tf$shape(input)[1], as.integer(num_params))) #this will take the current space up to the num_params -1
       logit = tf$clip_by_value(logit_crude, eps, 1 - eps)
 
-      ll = -tf$math$log(logit)*tf$slice(target, begin = list(0L, as.integer(index_type-1)), size = list(tf$shape(input)[1], as.integer(num_params)))
+      ll = -tf$math$log(logit)*tf$slice(target, begin = list(0L, as.integer(index_y)), size = list(tf$shape(input)[1], as.integer(num_params)))
       #This is the typical negative log liklihood for categorical,
 
-      cat_loss_LL[[index_type]]= tf$reduce_mean(ll)
-      index_x = index_x + num_params # We move over based on the number of params used for that given categorical variable
+      #Now take sum
+      sum_across_rows = tf$reduce_sum(ll, axis = 1L)
+      cat_loss_LL[[index_type]]= tf$reduce_mean(sum_across_rows)
+      index_x = index_x + as.integer(num_params) # We move over based on the number of params used for that given categorical variable
+      index_y = index_y + as.integer(num_params)
     }
     else {
       stop("Unknown:", distribution)  #A measure to catch errors
@@ -87,6 +88,6 @@ lossbasedondist = function(input, feat_dist, target,weighted=0, recon_weights){ 
   weighted_groups   <- tf$multiply(group_weights, group_losses)
   total_loss        <- tf$reduce_sum(weighted_groups, axis = 0L)
 
-  message(paste("Loss - total", total_loss))
+  print(paste("Loss - total", total_loss))
   return(list(total_loss,cont_group_loss,bin_group_loss,cat_group_loss) )  }
 
